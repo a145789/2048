078a28dbfe1fc62b7228ada1d911c6129c58911b
clencat <37403253+a145789@users.noreply.github.com>
2022-07-18 20:07:15 +0800

    refactor: 重构

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <script lang="ts" setup>
 import vTouchdir, { Direction } from 'vtouchdir'
 import { computed, reactive, ref } from 'vue'
-import Color from 'color'
 
 import { useToast } from 'vue-toast-notification'
 import 'vue-toast-notification/dist/theme-sugar.css'
-import { BG_COLOR, GRID_SIZE, INIT_BG_COLOR, WIN_COUNT } from './constants'
-
-type CountType = keyof typeof BG_COLOR
-type ColorType = typeof BG_COLOR[CountType]
+import { BG_COLOR, GRID_SIZE, GameStatus, INIT_BG_COLOR } from './constants'
+import type { CountType, Grid } from './utils'
+import {
+  type ColorType,
+  getInitGame,
+  getIsEmptySquare,
+  getIsWinOrLoss,
+  getRandomCount,
+  getTransformColor,
+} from './utils'
 
 const toast = useToast({ position: 'top' })
 
-const isOver = ref(false)
-const grid = ref<
-  { count: CountType | null; bg: ColorType | typeof INIT_BG_COLOR }[][]
->([])
+const gameStatus = ref(GameStatus.normal)
+const $grid = ref<Grid>([])
 const record = reactive({
   score: 0,
   step: 0,
 })
 
 const currentMaxCount = computed(() => {
-  return grid.value.reduce<CountType>((initCount, line) => {
+  return $grid.value.reduce<CountType>((initCount, line) => {
     return line.reduce((p, c) => {
-      if (isEmptySquare(c.count)) {
+      if (getIsEmptySquare(c.count)) {
         return p
       }
       return c.count > p ? c.count : p
@@ -33,103 +36,54 @@ const currentMaxCount = computed(() => {
 })
 const tone = computed(() => ({
   mainColor: `${BG_COLOR[currentMaxCount.value]}`,
-  inverseColor: `${transformColor(BG_COLOR[currentMaxCount.value])}`,
+  inverseColor: `${getTransformColor(BG_COLOR[currentMaxCount.value])}`,
 }))
 
-function transformColor(color: ColorType, rotate = 180) {
-  return Color(color).rotate(rotate).toString()
-}
-function isEmptySquare(count: CountType | null): count is null {
-  return count === null
-}
-function aroundHasSameCount(y: number, x: number) {
-  const theGrid = grid.value
-  const count = theGrid[y][x].count
-  if (
-    count === theGrid[y - 1]?.[x].count ||
-    count === theGrid[y + 1]?.[x].count ||
-    count === theGrid[y][x - 1]?.count ||
-    count === theGrid[y][x + 1]?.count
-  ) {
-    return true
-  }
-  return false
-}
-function isWinOrLoss() {
-  let i = 0
-  for (let y = 0; y < GRID_SIZE.axisY; y++) {
-    for (let x = 0; x < GRID_SIZE.axisX; x++) {
-      if (grid.value[y][x].count === WIN_COUNT) {
-        toast.success('恭喜你，你赢了!')
-        isOver.value = true
-        return
-      }
-      if (!isEmptySquare(grid.value[y][x].count) && !aroundHasSameCount(y, x)) {
-        i++
-      }
-    }
-  }
+function showGameStatusToast(status: GameStatus) {
+  toast.clear()
+  switch (status) {
+    case GameStatus.win:
+      toast.success('恭喜你，你赢了!')
+      break
+    case GameStatus.loss:
+      toast.default('貌似无路可走了!')
+      break
 
-  if (i === GRID_SIZE.axisX * GRID_SIZE.axisY) {
-    isOver.value = true
-    toast.default('貌似无路可走了!')
+    default:
+      break
   }
-}
-function genSquare(y: number, x: number, count: CountType | null) {
-  grid.value[y][x].count = count
-  grid.value[y][x].bg = isEmptySquare(count) ? INIT_BG_COLOR : BG_COLOR[count]
 }
 
-function genGame() {
-  grid.value = Array.from({ length: GRID_SIZE.axisY }, () =>
-    Array.from({ length: GRID_SIZE.axisX }, () => ({
-      count: null,
-      bg: INIT_BG_COLOR,
-    }))
-  )
+function updateSquare(y: number, x: number, count: CountType | null) {
+  $grid.value[y][x].count = count
+  $grid.value[y][x].bg = getIsEmptySquare(count)
+    ? INIT_BG_COLOR
+    : BG_COLOR[count]
 }
-function genRandomCount(num: number) {
-  if (
-    grid.value.every((line) =>
-      line.every((square) => !isEmptySquare(square.count))
-    )
-  ) {
-    return
-  }
-  let i = 0
-  while (i < num) {
-    const y = Math.floor(Math.random() * GRID_SIZE.axisY)
-    const x = Math.floor(Math.random() * GRID_SIZE.axisX)
-    if (grid.value[y][x].count !== null) {
-      continue
-    }
-    genSquare(y, x, Math.random() > 0.5 ? 2 : 4)
 
-    i++
-  }
-}
 function moveHandle(
   [currentY, currentX]: [number, number],
   [nextY, nextX]: [number, number]
 ) {
-  if (isEmptySquare(grid.value[nextY][nextX].count)) {
-    genSquare(nextY, nextX, grid.value[currentY][currentX].count)
-    genSquare(currentY, currentX, null)
+  if (getIsEmptySquare($grid.value[nextY][nextX].count)) {
+    updateSquare(nextY, nextX, $grid.value[currentY][currentX].count)
+    updateSquare(currentY, currentX, null)
     return false
   }
-  if (grid.value[currentY][currentX].count === grid.value[nextY][nextX].count) {
-    const count = (grid.value[currentY][currentX].count! << 1) as CountType
-    genSquare(nextY, nextX, count)
-    genSquare(currentY, currentX, null)
+  if (
+    $grid.value[currentY][currentX].count === $grid.value[nextY][nextX].count
+  ) {
+    const count = ($grid.value[currentY][currentX].count! << 1) as CountType
+    updateSquare(nextY, nextX, count)
+    updateSquare(currentY, currentX, null)
 
     record.score += count
   }
   return true
 }
 function move(dir: Direction) {
-  if (isOver.value) {
-    toast.clear()
-    toast.default('貌似无路可走了!')
+  if (gameStatus.value !== GameStatus.normal) {
+    showGameStatusToast(gameStatus.value)
     return
   }
 
@@ -137,7 +91,7 @@ function move(dir: Direction) {
     case Direction.UP: {
       for (let y = 0; y < GRID_SIZE.axisY; y++) {
         for (let x = 0; x < GRID_SIZE.axisY; x++) {
-          if (isEmptySquare(grid.value[y][x].count)) {
+          if (getIsEmptySquare($grid.value[y][x].count)) {
             continue
           }
 
@@ -160,7 +114,7 @@ function move(dir: Direction) {
     case Direction.DOWN: {
       for (let y = GRID_SIZE.axisY - 1; y >= 0; y--) {
         for (let x = 0; x < GRID_SIZE.axisY; x++) {
-          if (isEmptySquare(grid.value[y][x].count)) {
+          if (getIsEmptySquare($grid.value[y][x].count)) {
             continue
           }
 
@@ -183,7 +137,7 @@ function move(dir: Direction) {
     case Direction.LEFT: {
       for (let x = 0; x < GRID_SIZE.axisY; x++) {
         for (let y = 0; y < GRID_SIZE.axisY; y++) {
-          if (isEmptySquare(grid.value[y][x].count)) {
+          if (getIsEmptySquare($grid.value[y][x].count)) {
             continue
           }
 
@@ -206,7 +160,7 @@ function move(dir: Direction) {
     case Direction.RIGHT: {
       for (let x = GRID_SIZE.axisY - 1; x >= 0; x--) {
         for (let y = 0; y < GRID_SIZE.axisY; y++) {
-          if (isEmptySquare(grid.value[y][x].count)) {
+          if (getIsEmptySquare($grid.value[y][x].count)) {
             continue
           }
 
@@ -231,9 +185,18 @@ function move(dir: Direction) {
       break
   }
 
-  genRandomCount(1)
+  const [
+    {
+      position: [y, x],
+      count,
+    },
+  ] = getRandomCount(1, $grid.value)
+  updateSquare(y, x, count)
+
+  gameStatus.value = getIsWinOrLoss($grid.value)
+  showGameStatusToast(gameStatus.value)
+
   record.step++
-  isWinOrLoss()
 }
 
 function keyDownHandling(e: KeyboardEvent) {
@@ -261,11 +224,17 @@ function registerEvent() {
 }
 
 function beginGame() {
-  isOver.value = false
+  gameStatus.value = GameStatus.normal
   record.step = 0
   record.score = 0
-  genGame()
-  genRandomCount(2)
+  $grid.value = getInitGame()
+
+  for (const {
+    position: [y, x],
+    count,
+  } of getRandomCount(2, $grid.value)) {
+    updateSquare(y, x, count)
+  }
 }
 function init() {
   beginGame()
@@ -294,7 +263,7 @@ init()
       }"
     >
       <div
-        v-for="(line, index) of grid"
+        v-for="(line, index) of $grid"
         :key="index"
         class="grid grid-cols-4 grid-rows-1 gap-8px"
       >
@@ -304,7 +273,7 @@ init()
           class="w-72px h-72px text-light-1009 flex items-center justify-center text-28px font-bold"
           :class="`bg-${bg}`"
           :style="{
-            color: bg !== INIT_BG_COLOR ? transformColor(bg as ColorType, 160) : '',
+            color: bg !== INIT_BG_COLOR ? getTransformColor(bg as ColorType, 160) : '',
           }"
         >
           {{ count }}
